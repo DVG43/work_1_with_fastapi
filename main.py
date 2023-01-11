c7e335dbbbf1ea6d971ce752b255614605952cf8
Dmitrii <dvgaluta@gmail.com>
2023-01-12 00:47:42 +0300

    commit_3_с валидацией данных через pydantic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from shimas import Book
 
 app = FastAPI()
 
@@ -11,4 +12,13 @@ def home():
 @app.get('/{pk}')
 def get_item(pk: int, q: int = None):
     return {"key": pk, "q": q}
+
+
+@app.get('/user/{pk}/item/{item}/')
+def get_user_item(pk: int, item: str):
+    return {"user": pk, "item": item}
+
+@app.post('/book')
+def create_book(item: Book):
+    return item
 
